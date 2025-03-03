e1ff9a2c6b61b413b63c770319b8286a06fd311b
Salma <salmamh315@gmail.com>
2025-03-03 13:15:15 +0300

    import useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext } from 'react';
+import { useState, createContext, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
 // Components
@@ -66,7 +66,7 @@ const App = () => {
             <>
             <Route path="/" element={<Dashboard user={user} />} />
             <Route path="/tasks" element={<TaskList tasks={tasks}/>} />
-            <Route path="/tasks/:taskId" element={<TasksDetails handleDeleteTask={handleDeleteTask}/>} />
+            <Route path="/tasks/:taskId" element={<TaskDetails handleDeleteTask={handleDeleteTask}/>} />
             <Route path="/tasks/new" element={<TaskForm handleAddTask={handleAddTask} />} />
             <Route path="/tasks/:taskId/edit" element={<TaskForm handleUpdateTask={handleUpdateTask} />}
 />
