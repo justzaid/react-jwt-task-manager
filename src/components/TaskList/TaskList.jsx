d3c29e7fcd8b4bec1fd7cace2f3a933f71fef96b
Salma <salmamh315@gmail.com>
2025-03-05 11:37:39 +0300

    owner task css

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,12 +1,23 @@
 import { Link } from 'react-router-dom';
 import styles from './TaskList.module.css';
+import { AuthedUserContext } from '../../App';
+import { useContext } from 'react';
+
 
 const TaskList = (props) => {
+   const user = useContext(AuthedUserContext);
   return (
     <div className={styles.board}>
       {props.tasks && props.tasks.length > 0 ? (
         props.tasks.map((task) => (
-          <div key={task._id} className={styles.taskCard}>
+         <div key={task._id}
+          // className={styles.taskCard}
+           
+          className={`${styles.taskCard} ${
+            task.author._id === user._id ? styles.ownerTask : ""
+          }`}
+          
+          >
             <header className={styles.cardHeader}>
               <h3>{task.title}</h3>
               <p className={styles.taskInfo}>
