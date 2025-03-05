c7d7198cbbf560ea7a0a0e636fc2ec645f01c071
justzxid <zaidmk96@gmail.com>
2025-03-05 23:16:48 +0300

    Updated Dashboard text, Landing page button border radius, flex alignment, and image size, Task list layout into vertical displays

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -3,44 +3,111 @@ import styles from './TaskList.module.css';
 import { AuthedUserContext } from '../../App';
 import { useContext } from 'react';
 
-
 const TaskList = (props) => {
-   const user = useContext(AuthedUserContext);
+  const user = useContext(AuthedUserContext);
+
+  // Group tasks by category
+  const taskCategories = {
+    'Not started': [],
+    'In process': [],
+    'Done': []
+  };
+
+  props.tasks?.forEach((task) => {
+    taskCategories[task.category]?.push(task);
+  });
+
   return (
     <div className={styles.board}>
-      {props.tasks && props.tasks.length > 0 ? (
-        props.tasks.map((task) => (
-         <div key={task._id}
-          // className={styles.taskCard}
-           
-          className={`${styles.taskCard} ${
-            task.author._id === user._id ? styles.ownerTask : ""
-          }`}
-            
-          >
-            <header className={styles.cardHeader} 
-            
+      <div className={styles.column}>
+        <div className={styles.columnHeader}>Not Started</div>
+        <div className={styles.taskContainer}>
+          {taskCategories['Not started']?.map((task) => (
+            <div
+              key={task._id}
+              className={`${styles.taskCard} ${
+                task.author._id === user._id ? styles.ownerTask : ''
+              }`}
             >
-           
-              <h3>{task.title}</h3>
-              <p className={styles.taskInfo}>
-                {task.author.username} posted on{' '}
-                {new Date(task.createdAt).toLocaleDateString()}
-              </p>
-            </header>
-            <div className={styles.cardBody}>
-              <p>{task.text}</p>
+              <header className={styles.cardHeader}>
+                <h3>{task.title}</h3>
+                <p className={styles.taskInfo}>
+                  {task.author.username} posted on{' '}
+                  {new Date(task.createdAt).toLocaleDateString()}
+                </p>
+              </header>
+              <div className={styles.cardBody}>
+                <p>{task.text}</p>
+              </div>
+              <footer className={styles.cardFooter}>
+                <Link to={`/tasks/${task._id}`} className={styles.showMoreButton}>
+                  Show More
+                </Link>
+              </footer>
             </div>
-            <footer className={styles.cardFooter}>
-              <Link to={`/tasks/${task._id}`} className={styles.showMoreButton}>
-                Show More
-              </Link>
-            </footer>
-          </div>
-        ))
-      ) : (
-        <p>No tasks available</p>
-      )}
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.column}>
+        <div className={styles.columnHeader}>In Process</div>
+        <div className={styles.taskContainer}>
+          {taskCategories['In process']?.map((task) => (
+            <div
+              key={task._id}
+              className={`${styles.taskCard} ${
+                task.author._id === user._id ? styles.ownerTask : ''
+              }`}
+            >
+              <header className={styles.cardHeader}>
+                <h3>{task.title}</h3>
+                <p className={styles.taskInfo}>
+                  {task.author.username} posted on{' '}
+                  {new Date(task.createdAt).toLocaleDateString()}
+                </p>
+              </header>
+              <div className={styles.cardBody}>
+                <p>{task.text}</p>
+              </div>
+              <footer className={styles.cardFooter}>
+                <Link to={`/tasks/${task._id}`} className={styles.showMoreButton}>
+                  Show More
+                </Link>
+              </footer>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.column}>
+        <div className={styles.columnHeader}>Done</div>
+        <div className={styles.taskContainer}>
+          {taskCategories['Done']?.map((task) => (
+            <div
+              key={task._id}
+              className={`${styles.taskCard} ${
+                task.author._id === user._id ? styles.ownerTask : ''
+              }`}
+            >
+              <header className={styles.cardHeader}>
+                <h3>{task.title}</h3>
+                <p className={styles.taskInfo}>
+                  {task.author.username} posted on{' '}
+                  {new Date(task.createdAt).toLocaleDateString()}
+                </p>
+              </header>
+              <div className={styles.cardBody}>
+                <p>{task.text}</p>
+              </div>
+              <footer className={styles.cardFooter}>
+                <Link to={`/tasks/${task._id}`} className={styles.showMoreButton}>
+                  Show More
+                </Link>
+              </footer>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
